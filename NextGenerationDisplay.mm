666dec74b761400791a84c92cc231286c144e4f2
ken <xiayang_yanshan@163.com>
2020-07-24 19:36:20 +0800

    add next display

---
+++ NextGenerationDisplay.mm
@@ -0,0 +1,62 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1592963372155" ID="ID_577278204" MODIFIED="1592963691171">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4" face="&#x6977;&#x4f53;" color="#ff0000">&#19979;&#19968;&#20195;&#26174;&#31034;&#31995;&#32479;</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1592963484755" ID="ID_931970435" MODIFIED="1592964256157" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="5" face="&#x5b8b;&#x4f53;">&#20809;&#26426;&#31995;&#32479;</font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1592963540265" ID="ID_1871838608" MODIFIED="1592963548533" TEXT="Hololens2"/>
+<node CREATED="1592964118712" ID="ID_1900921732" MODIFIED="1592964238469">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4" face="&#x96b6;&#x4e66;">LCOS &#20809;&#26426;</font></b>
+    </p>
+    <p>
+      Liquid Crystal on Silicon &#30789;&#22522;&#28082;&#26230;&#25216;&#26415;
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1595382980566" ID="ID_1169199962" MODIFIED="1595383031151">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>&#28082;&#26230;&#26174;&#31034;&#25216;&#26415;</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1592964130594" ID="ID_572813486" MODIFIED="1592964133927" TEXT="FLCOS&#x5149;&#x673a;"/>
+<node CREATED="1592964137950" ID="ID_1409315038" MODIFIED="1592964140965" TEXT="DLP &#x5149;&#x673a;"/>
+<node CREATED="1592964144669" ID="ID_138026738" MODIFIED="1592964147471" TEXT="SBL &#x5149;&#x673a;"/>
+</node>
+<node CREATED="1592963497080" ID="ID_1535897177" MODIFIED="1592963502797" POSITION="left" TEXT="&#x5149;&#x5b66;&#x955c;&#x7247;&#x7cfb;&#x7edf;"/>
+<node CREATED="1592963506794" ID="ID_837416884" MODIFIED="1592963518187" POSITION="right" TEXT="AR&#x773c;&#x955c;&#x8bc4;&#x4ef7;&#x53c2;&#x6570;"/>
+<node CREATED="1592963522547" ID="ID_1956502235" MODIFIED="1592963525568" POSITION="left" TEXT="&#x5168;&#x606f;&#x6280;&#x672f;"/>
+</node>
+</map>
